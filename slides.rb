ee44b2fe393b8760fda0509aa226c94cac4825bf
Andrew Mason <mason@case.edu>
2015-03-10 03:10:39 -0400

    typo fix. add support for attributes on the slide tags

--- slides.rb
+++ slides.rb
@@ -1,12 +1,17 @@
 module Jekyll
   module Tags
     class SlideTag < Liquid::Block
+      def initialize(tag_name, args, tokens)
+        super
+        @section_attributes = args
+      end
+
       def render(context)
         site = context.registers[:site]
         converter = site.getConverterImpl(::Jekyll::Converters::Markdown)
 
-        content = coverter.convert(super(context))
-        "<section>#{content}</section>"
+        content = converter.convert(super(context))
+        "<section #{@section_attributes}>#{content}</section>"
       end
     end
   end
